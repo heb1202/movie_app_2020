c9e2c7e583c298cb00a7ce92d01c5ede55754bff
Chris Kwon <heb1202@naver.com>
2020-07-22 19:38:10 +0900

    Updates

---
+++ precache-manifest.755dff53941d268f321ab2626ab61722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e6165c9a3b093404ad908452e4271f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2c91ecaa5bfc2d9ec20f",
+    "url": "/movie_app_2020/static/css/main.6f2d31eb.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c91ecaa5bfc2d9ec20f",
+    "url": "/movie_app_2020/static/js/main.2989c01c.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
