596b97f29e66a09b5d7f7dfa7b0b05d07b4367c7
Chris Kwon <heb1202@naver.com>
2020-07-23 15:40:30 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.3931337a3a865f41709438efbce0d58a.js"
+  "/movie_app_2020/precache-manifest.15edc1b08c9572baa4326b016716a1f0.js"
 );
 
 self.addEventListener('message', (event) => {
